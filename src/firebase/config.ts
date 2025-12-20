eaf0d2888324770f0fbf06f2b8af321f62be6d1f
Ali Farzadpour <dev@omidanai.com>
2025-12-20 17:30:12 +0000

    1. The Mismatch: Update your Firebase Config Your browser logs show your

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-7576922301-bac28",
   "appId": "1:785987975672:web:3d2a2404cd4168c3ffbe0d",
-  "storageBucket": "studio-811444605-7ef2a.appspot.com",
+  "storageBucket": "studio-811444605-7ef2a.firebasestorage.app",
   "apiKey": "AIzaSyCbDubxoArupHAz2jquVoNeB6j8vbZcY-8",
   "authDomain": "studio-7576922301-bac28.firebaseapp.com",
   "measurementId": "",
