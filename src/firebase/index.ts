e25a4d4d771f6a1c062a4c7d6a2c8962b9eec286
Ali Farzadpour <dev@omidanai.com>
2025-12-20 19:01:32 +0000

    ## Error Type Console FirebaseError
    ## Error Message
    Missing or insuffi

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -43,7 +43,7 @@ export * from './provider';
 export * from './client-provider';
 export * from './firestore/use-collection';
 export * from './firestore/use-doc';
-export * from './non-blocking-login';
 export * from './auth/use-user';
 export * from './errors';
 export * from './error-emitter';
+export * from './non-blocking-login';
