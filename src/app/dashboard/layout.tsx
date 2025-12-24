b2b4585023044775e85e828ac67f2772873d04b9
Ali Farzadpour <dev@omidanai.com>
2025-12-24 00:37:25 +0000

    we need vertical scroll bar to see very thing bottom of the page is miss

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -15,7 +15,7 @@ export default function DashboardLayout({
   return (
     <SidebarProvider>
       <AppSidebar />
-      <SidebarInset>
+      <SidebarInset className="flex flex-col">
         <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4 bg-white transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
           <SidebarTrigger className="-ml-1" />
           <Separator orientation="vertical" className="mr-2 h-4" />
