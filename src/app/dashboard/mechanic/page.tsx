56de5610f9c2e263a1e205cbca2c9d3e86429946
Ali Farzadpour <dev@omidanai.com>
2025-12-25 02:45:03 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve './

---
+++ src/app/dashboard/mechanic/page.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+import { BulkReconciler } from '@/components/dashboard/mechanic/BulkReconciler';
+
+export default function CategoryMechanicPage() {
+  return <BulkReconciler />;
+}
