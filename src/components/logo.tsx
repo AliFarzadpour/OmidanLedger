937e39147e59e4845872b32ddca671bb49cd91f4
Ali Farzadpour <dev@omidanai.com>
2026-01-06 01:10:54 +0000

    Here is the path for the logo: https://firebasestorage.googleapis.com/v0

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -13,7 +13,7 @@ export function Logo({
   return (
     <div className={cn('flex items-center', className)}>
       <Image
-        src="/logo.png"
+        src="https://firebasestorage.googleapis.com/v0/b/studio-7576922301-bac28.firebasestorage.app/o/logos%2FOmidanLedger%20logo%20Rightside%20trimed.png?alt=media&token=80772977-26b5-4080-b0f4-30eccf9cc323"
         alt="OmidanLedger logo"
         width={180}
         height={40}
