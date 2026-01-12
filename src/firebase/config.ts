dd6d8f083d55b55edb5229143d6d5b2def1388d2
Ali Farzadpour <dev@omidanai.com>
2026-01-12 06:26:58 +0000

    all api keys are located in .env.local , don't create a new key. use the

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,7 +2,7 @@ export const firebaseConfig = {
   "projectId": "studio-7576922301-bac28",
   "appId": "1:785987975672:web:3d2a2404cd4168c3ffbe0d",
   "storageBucket": "studio-7576922301-bac28.firebasestorage.app",
-  "apiKey": "AIzaSyCbDubxoArupHAz2jquVoNeB6j8vbZcY-8",
+  "apiKey": process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   "authDomain": "studio-7576922301-bac28.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "785987975672"
