eac2daf78ab6d4f7e69472b8a1744005f3372e19
Ali Farzadpour <dev@omidanai.com>
2026-02-04 18:33:05 +0000

    I am seeing the following error. how should we proceed fixing it:  POST

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,9 @@ const nextConfig: NextConfig = {
     // your project has type errors.
     ignoreBuildErrors: true,
   },
+  experimental: {
+    allowedDevOrigins: ["https://*.cloudworkstations.dev"],
+  },
   images: {
     remotePatterns: [
       {
